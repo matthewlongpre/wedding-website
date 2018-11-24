d6b17db83a94d92c8c6c2b0dce7136b2da2ed889
Matt Longpre <MacPre@Matts-MacBook-Pro-2.local>
2018-11-24 15:22:12 -0800

    Home back to functional component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,37 +5,31 @@ import { AppContextConsumer } from '../components/AppContext';
 import MALogo from '../assets/new.svg';
 import SVG from 'react-inlinesvg';
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.bgElement = null;
-  }
-  render() {
-    return (
-      <AppContextConsumer>
-        {context =>
-          <div ref={bgElement => this.bgElement = bgElement} className={`bg-1 page-home page-background w-100 h-100 flex flex-direction-column justify-content-center align-items-center`}>
-            <div className="page-content p-40 position-relative flex flex-direction-column justify-content-center align-items-center">
+const Home = (props) => {
+  return (
+    <AppContextConsumer>
+      {context =>
+        <div className={`bg-1 page-home page-background w-100 h-100 flex flex-direction-column justify-content-center align-items-center`}>
+          <div className="page-content p-40 position-relative flex flex-direction-column justify-content-center align-items-center">
 
-              <SVG
-                className="ma-logo"
-                src={MALogo}
-                onLoad={(src) => {
-                  this.props.svgLoaded(src);
-                }}
-                cacheGetRequests
-              >
-                <img src={MALogo} />
-              </SVG>
+            <SVG
+              className="ma-logo"
+              src={MALogo}
+              onLoad={(src) => {
+                props.svgLoaded(src);
+              }}
+              cacheGetRequests
+            >
+              <img src={MALogo} />
+            </SVG>
 
-              <h1 className="text-uppercase text-italic letter-spacing-2 f-1 m-0 pv-2">September 7, 2019</h1>
-              <button className="button rsvp font-raleway">
-                <Link to="/rsvp/">RSVP</Link>
-              </button>
-            </div>
-          </div>}
-      </AppContextConsumer>
-    );
-  }
+            <h1 className="text-uppercase text-italic letter-spacing-2 f-1 m-0 pv-2">September 7, 2019</h1>
+            <button className="button rsvp font-raleway">
+              <Link to="/rsvp/">RSVP</Link>
+            </button>
+          </div>
+        </div>}
+    </AppContextConsumer>
+  );
 }
 export default Home;
