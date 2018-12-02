19077f548a06d74a12d1f5d6109ab51ea6e83707
Matt Longpre <MacPre@Matts-MacBook-Pro-2.local>
2018-12-02 14:13:49 -0800

    Sign in with popup

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import firebase from '../firebase';
 
+import ReactTable from 'react-table';
+import "react-table/react-table.css";
+
 const provider = new firebase.auth.GoogleAuthProvider();
 
+
 class Admin extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authenticated: false,
+      data: []
+    }
+
+    this.databaseRef = firebase.database().ref('rsvps');
+
+  }
 
   componentDidMount() {
-    firebase.auth().signInWithPopup(provider).then(function (result) {
+
+    firebase.auth().signInWithPopup(provider).then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const token = result.credential.accessToken;
       // The signed-in user info.
       const user = result.user;
-      // ...
-    }).catch(function (error) {
+
+      this.authSuccess(token, user);
+
+    }).catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -25,10 +43,57 @@ class Admin extends React.Component {
 
       // ...
     });
+
+    this.databaseRef.on('value', (item) => {
+      const data = Object.values(item.val());
+      this.setState({
+        data: data
+      })
+    });
+  }
+
+  authSuccess(token, user) {
+    this.setState({
+      authenticated: true,
+      token: token,
+      user: user
+    });
   }
 
   render() {
-    return <div>Admin</div>
+    const { authenticated, data } = this.state;
+
+    if (!authenticated) return <div></div>
+
+    return (
+      <div className="flex w-100 h-100 table-wrap flex-direction-column justify-content-center align-items-center" >
+        <ReactTable className="table" data={data}
+          columns={[
+            {
+              Header: "Name",
+              columns: [
+                {
+                  Header: "First Name",
+                  accessor: "firstName"
+                },
+                {
+                  Header: "Last Name",
+                  accessor: "lastName"
+                },
+                {
+                  Header: "Email",
+                  accessor: "email"
+                },
+                {
+                  Header: "Message",
+                  accessor: "message"
+                }
+              ]
+            }
+          ]}
+        />
+      </div>
+    );
   }
 }
 
