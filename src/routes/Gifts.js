371b435436abcb59a556f4b695f3db3675b80185
Matt Longpre <mlongpre@itgroove.ca>
2019-01-13 13:39:19 -0800

    Gifts content update

--- src/routes/Gifts.js
+++ src/routes/Gifts.js
@@ -11,9 +11,20 @@ const Gifts = props => {
         className={`page-background w-100 h-100 flex flex-direction-column justify-content-center align-items-center background-cover`}
       >
         <div className="page-content p-40 position-relative flex flex-direction-column justify-content-center align-items-center">
-          <h1 className="font-blithe m-0 f-3">
-            No presents, please. Just your presence.
+          <h1 className="font-blithe m-0 f-3 text-center">
+            No presents, please.<br /> Just your presence.
           </h1>
+          <div className="text-left mt-20">
+            <p>
+              Honestly, your attendance is the perfect gift as far as we're
+              concerned.
+            </p>
+            <p>
+              But if you <em>really</em> feel like giving us a gift, we would
+              appreciate <br />
+              <strong>a contribution to our home/honeymoon fund</strong>.
+            </p>
+          </div>
         </div>
       </div>
     </FadeIn>
