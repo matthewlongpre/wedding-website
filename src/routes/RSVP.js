dcec6005d7f5f2a641819038b23960e35786508d
Matt Longpre <mlongpre@itgroove.ca>
2019-01-12 20:23:43 -0800

    Form updates

--- src/routes/RSVP.js
+++ src/routes/RSVP.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import firebase from '../firebase';
-import { FormModal } from '../components/FormModal';
-import '../styles/form.css';
-import MALogo from '../assets/m+a-2.svg';
-import SVG from 'react-inlinesvg';
+import React from "react";
+import firebase from "../firebase";
+import { FormModal } from "../components/FormModal";
+import "../styles/form.css";
+import MALogo from "../assets/m+a-2.svg";
+import SVG from "react-inlinesvg";
 
 class RSVP extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      rsvp: ''
-    }
+      firstName: "",
+      lastName: "",
+      email: "",
+      rsvp: ""
+    };
 
     this._handleChange = this._handleChange.bind(this);
     this._handleSubmit = this._handleSubmit.bind(this);
-    this.databaseRef = firebase.database().ref('rsvps');
-
+    this.databaseRef = firebase.database().ref("rsvps");
   }
 
   _handleChange(event) {
@@ -41,10 +40,10 @@ class RSVP extends React.Component {
     const submission = rsvp;
     submission.timeStamp = Date.now();
     const newRSVPRef = this.databaseRef.push();
-    newRSVPRef.set(rsvp, (error) => {
+    newRSVPRef.set(rsvp, error => {
       if (error) {
-        alert(error)
-        console.error('Failed', error);
+        alert(error);
+        console.error("Failed", error);
       } else {
         this._handleFormSubmitSuccessful();
       }
@@ -60,7 +59,7 @@ class RSVP extends React.Component {
   }
 
   _handleFinishedClick() {
-    this.props.history.push('/');
+    this.props.history.push("/");
     this.props._formFinished();
   }
 
@@ -80,53 +79,115 @@ class RSVP extends React.Component {
     return (
       <div className="page-home page-background w-100 h-100 flex justify-content-center">
         <div className="page-content w-100 pt-100 position-relative text-center">
+          <SVG className="ma-logo rsvp" src={MALogo} cacheGetRequests />
 
-          <SVG
-            className="ma-logo rsvp"
-            src={MALogo}
-            cacheGetRequests
+          <form
+            className="form float-label text-left"
+            id="intro-form"
+            onSubmit={this._handleSubmit}
           >
-          </SVG>
-
-          <h4 className="text-italic">
-            Please fill out this form per individual guest.
-          </h4>
-
-          <form className="form float-label" id="intro-form" onSubmit={this._handleSubmit}>
+            <h4 className="text-italic">
+              Please fill out this form per individual guest.
+            </h4>
 
             <div className="control">
-              <input className="font-raleway text-uppercase text-italic letter-spacing-1 f-1" type="text" name="firstName" placeholder="First name" required id="firstName" onChange={this._handleChange} />
+              <input
+                className="font-raleway text-uppercase text-italic letter-spacing-1 f-1"
+                type="text"
+                name="firstName"
+                placeholder="First name"
+                required
+                id="firstName"
+                onChange={this._handleChange}
+              />
               <label htmlFor="firstName">First name:</label>
             </div>
 
             <div className="control">
-              <input className="font-raleway text-uppercase text-italic letter-spacing-1 f-1" type="text" name="lastName" placeholder="Last name" id="lastName" required onChange={this._handleChange} />
+              <input
+                className="font-raleway text-uppercase text-italic letter-spacing-1 f-1"
+                type="text"
+                name="lastName"
+                placeholder="Last name"
+                id="lastName"
+                required
+                onChange={this._handleChange}
+              />
               <label htmlFor="lastName">Last name:</label>
             </div>
 
             <div className="control">
-              <input className="font-raleway text-uppercase text-italic letter-spacing-1 f-1" type="text" name="email" placeholder="Email" id="email" required onChange={this._handleChange} />
+              <input
+                className="font-raleway text-uppercase text-italic letter-spacing-1 f-1"
+                type="text"
+                name="email"
+                placeholder="Email"
+                id="email"
+                required
+                onChange={this._handleChange}
+              />
               <label htmlFor="email">Email:</label>
             </div>
 
             <h4 className="text-italic mb-0">Will you be attending?</h4>
 
             <div className="input-group radio">
-              <input id="radio1" name="rsvp" type="radio" required onChange={this._handleChange} value="accepted" />
+              <input
+                id="radio1"
+                name="rsvp"
+                type="radio"
+                required
+                onChange={this._handleChange}
+                value="accepted"
+              />
               <label htmlFor="radio1">Yes, with pleasure!</label>
             </div>
 
             <div className="input-group radio">
-              <input id="radio2" name="rsvp" type="radio" className="radio-decline" required onChange={this._handleChange} value="declined" />
-              <label htmlFor="radio2" className="label-decline">No, I'll celebrate from afar.</label>
+              <input
+                id="radio2"
+                name="rsvp"
+                type="radio"
+                className="radio-decline"
+                required
+                onChange={this._handleChange}
+                value="declined"
+              />
+              <label htmlFor="radio2" className="label-decline">
+                No, I'll celebrate from afar.
+              </label>
             </div>
 
             <div className="control">
-              <textarea className="font-raleway letter-spacing-1 f-1 message" type="text" name="message" placeholder="Leave us a message" id="message" onChange={this._handleChange} />
+              <textarea
+                className="font-raleway letter-spacing-1 f-1 message"
+                type="text"
+                name="message"
+                placeholder="Leave us a message"
+                id="message"
+                onChange={this._handleChange}
+              />
               <label htmlFor="message">Message:</label>
             </div>
 
-            <button className="button mb-40 font-raleway text-uppercase" type="submit">Submit</button>
+            <div className="control">
+              <textarea
+                className="font-raleway letter-spacing-1 f-1 message dietary-restrictions"
+                type="text"
+                name="dietaryRestrictions"
+                placeholder="Any dietary restrictions?"
+                id="dietaryRestrictions"
+                onChange={this._handleChange}
+              />
+              <label htmlFor="message">Any dietary restrictions?</label>
+            </div>
+
+            <button
+              className="button mb-40 font-raleway text-uppercase"
+              type="submit"
+            >
+              Submit
+            </button>
           </form>
         </div>
       </div>
