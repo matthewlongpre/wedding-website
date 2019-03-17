79a152a9ad91d303722551fddfb8ffebedb7c4d1
Matt Longpre <mlongpre@itgroove.ca>
2019-03-17 15:18:44 -0700

    Added min-height

--- src/components/FormModal.js
+++ src/components/FormModal.js
@@ -17,7 +17,7 @@ export const FormModal = (props) => {
         <div className="form-modal flex flex-direction-column justify-content-center align-items-center">
           <FormSuccess loadComplete={formSubmitSuccessful} />
           {formSubmitSuccessful &&
-            <div>
+            <div className="form-success-container">
               <h2 className="text-italic">{isAttending ? `Thanks ${firstName}, see you there!` : `Sorry you can't make it ${firstName}.`}</h2>
               <h4 className="text-italic">Do you have another guest to RSVP for?</h4>
               <button className="button mb-20 w-100 font-raleway text-uppercase" type="button" onClick={() => props._handleSubmitAnotherClick()}>Yes!</button>
